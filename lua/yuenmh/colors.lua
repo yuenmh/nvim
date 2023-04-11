edd5ed4a352e579f1222386b684b768139809f50
Evan Jones・原文軒 <48502664+yuenmh@users.noreply.github.com>
2023-04-11 16:21:23 -0700

    make inactive windows transparent

--- lua/yuenmh/colors.lua
+++ lua/yuenmh/colors.lua
@@ -1,8 +1,11 @@
+---Set the color scheme and then make the background transparent
+---@param color string
 function ColorMyPencils(color)
     print(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
