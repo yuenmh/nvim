632bae0d20d30bde8661caaf94fbd8680829c810
Evan Jones・原文軒 <48502664+yuenmh@users.noreply.github.com>
2023-04-15 21:14:15 -0700

    added utils for dev

--- lua/yuenmh/init.lua
+++ lua/yuenmh/init.lua
@@ -7,3 +7,20 @@ if require('yuenmh.packer') then
 else
     print("Failed to load packer, skipped loading plugins")
 end
+
+---Reload module
+---@param mod string
+---@return unknown
+function R(mod)
+    package.loaded[mod] = nil
+    local module = require(mod)
+    return module
+end
+
+---Print table
+---@param table table
+---@return table
+function P(table)
+    print(vim.inspect(table))
+    return table
+end
