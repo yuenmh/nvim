044eae8707899addf52059640af0f27c25405188
Evan Jones （原文軒） <48502664+evanj0@users.noreply.github.com>
2023-06-22 21:22:05 -0700

    add null ls and prettierd

--- lua/yuenmh/plugins.lua
+++ lua/yuenmh/plugins.lua
@@ -154,7 +154,19 @@ function M.plugins()
                 })
             end,
         },
-
+        {
+            'jose-elias-alvarez/null-ls.nvim',
+            requires = 'nvim-lua/plenary.nvim',
+            lazy = false,
+            config = function()
+                local null_ls = require('null-ls')
+                null_ls.setup({
+                    sources = {
+                        null_ls.builtins.formatting.prettierd,
+                    },
+                })
+            end,
+        },
 
         -- Cosmetic stuff
         {
