1aa3c002b7e38d693199d53a3610470b61755b8f
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-03-20 19:00:04 -0700

    nice remaps

--- init.lua
+++ init.lua
@@ -197,6 +197,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Move selected lines up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = '[J] Move selected line down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = '[K] Move selected line up' })
+
+-- Paste without overwriting paste buffer
+vim.keymap.set('v', 'p', 'P')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
