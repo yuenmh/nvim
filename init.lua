066764672ac43fc6a940fc711637bac6c40e3c7d
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-04-13 19:14:04 -0700

    add ts plugin

--- init.lua
+++ init.lua
@@ -976,6 +976,11 @@ require('lazy').setup({
             }
         end,
     },
+    {
+        'pmizio/typescript-tools.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+        opts = {},
+    },
 
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
