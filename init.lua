d444ce97c6381ef324a4fff80855fa59b9fef978
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2026-08-08 18:01:39 +0800

    switch to sync formatting

--- init.lua
+++ init.lua
@@ -125,7 +125,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 vim.api.nvim_create_autocmd('BufWritePre', {
     callback = function()
-        vim.lsp.buf.format { async = true }
+        vim.lsp.buf.format { async = false }
     end
 })
 
